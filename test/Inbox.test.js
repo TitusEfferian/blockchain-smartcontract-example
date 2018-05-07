3a6ad2e7743720758f156fae0eca5f5ac28b0c14
titusRalali <titus.efferian@ralali.com>
2018-05-07 16:23:47 +0700

    config local test network with ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,46 @@
 const assert = require('assert');
-const ganache = require('ganache-cli');//local etherum network
+const ganache = require('ganache-cli');//local etherum network,just for development purpose
 const Web3 = require('web3');//class
-
 const web3 = new Web3(ganache.provider());//instance
+
+beforeEach(() => {
+    //get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+
+    //use one of those account to deploy
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        
+    });
+});
+
+
+
+/* testing sample
+class Car {
+    park() {
+        return 'stopped';
+    } 
+    drive() {
+        return 'move';
+    }
+}
+let car;
+beforeEach(() => {
+    car = new Car();
+});
+
+describe('Car', () => {
+    it('can park', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('can drive', () => {
+        assert.equal(car.drive(), 'move');
+    })
+});*/
